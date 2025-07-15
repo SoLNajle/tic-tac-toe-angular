6b345574d5571e3e6b444ddfcb68380162bb1fc7
Sol Najle <sgnajle@gmail.com>
2025-07-15 16:19:18 +0200

    added hover animation and changed board content to add hover and value

--- src/app/components/game-board/game-board.ts
+++ src/app/components/game-board/game-board.ts
@@ -13,26 +13,27 @@ import { ConfettiService } from '../../services/confetti';
   standalone: true,
 })
 export class GameBoard {
-  constructor(private confettiService: ConfettiService) {}
+  constructor(private confettiService: ConfettiService) { 
+  }
   playerX: string = "X";
   playerO: string = "O";
   winner: string | null = null;
   isDraw: boolean = false;
   gameOver: boolean = false;
   currentPlayer: string = this.playerX;
-  board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""]
-  ];
+board = [
+  [{ value: '', hovered: false }, { value: '', hovered: false }, { value: '', hovered: false }],
+  [{ value: '', hovered: false }, { value: '', hovered: false }, { value: '', hovered: false }],
+  [{ value: '', hovered: false }, { value: '', hovered: false }, { value: '', hovered: false }]
+];
 
-  
+
   onCellClicked(row: number, col: number) {
     // Check if cell is empty
-    if (this.board[row][col] === '') {
+    if (this.board[row][col].value === '') {
       // Assign current player's value to the cell
-      this.board[row][col] = this.currentPlayer;
-      
+      this.board[row][col].value = this.currentPlayer;
+
       // Switch to the other player
       this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
     }
@@ -40,23 +41,23 @@ export class GameBoard {
   }
   checkWinnerbyRow() {
     for (let i = 0; i < 3; i++) {
-      if (this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2] && this.board[i][0] !== '') {
-        return this.board[i][0];
+      if (this.board[i][0].value === this.board[i][1].value && this.board[i][1].value === this.board[i][2].value && this.board[i][0].value !== '') {
+        return this.board[i][0].value;
       }
     }
     return null;
   }
   checkWinnerbyColumn() {
     for (let i = 0; i < 3; i++) {
-      if (this.board[0][i] === this.board[1][i] && this.board[1][i] === this.board[2][i] && this.board[0][i] !== '') {
-        return this.board[0][i];
+      if (this.board[0][i].value === this.board[1][i].value && this.board[1][i].value === this.board[2][i].value && this.board[0][i].value !== '') {
+        return this.board[0][i].value;
       }
     }
     return null;
   }
   checkWinnerbyDiagonal() {
-    if (this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2] && this.board[0][0] !== '') {
-      return this.board[0][0];
+    if (this.board[0][0].value === this.board[1][1].value && this.board[1][1].value === this.board[2][2].value && this.board[0][0].value !== '') {
+      return this.board[0][0].value;
     }
     return null;
   }
@@ -82,29 +83,43 @@ export class GameBoard {
     // Check if there is a winner
     const winner = this.checkWinner();
     if (winner) {
-      this.winner = winner;
+      this.winner = winner
       this.gameOver = true;
       this.confettiService.triggerPlayerConfetti(winner);
       return;
     }
     // Check for a draw
-    if (this.board.flat().every(cell => cell !== '')) {
+    if (this.board.flat().every(cell => cell.value !== '')) {
       this.isDraw = true;
       this.gameOver = true;
       return;
     }
 
   }
-  onRestartGameClicked() {
+
+  restartBoard() {
     this.board = [
-      ["", "", ""],
-      ["", "", ""],
-      ["", "", ""]
+      [{ value: '', hovered: false }, { value: '', hovered: false }, { value: '', hovered: false }],
+      [{ value: '', hovered: false }, { value: '', hovered: false }, { value: '', hovered: false }],
+      [{ value: '', hovered: false }, { value: '', hovered: false }, { value: '', hovered: false }]
     ];
+  }
+  onRestartGameClicked() {
+    this.restartBoard();
     this.currentPlayer = this.playerX;
     this.gameOver = false;
     this.winner = null;
     this.isDraw = false;
   }
- 
+  onCellHovered(row: number, col: number) {
+    console.log(`Cell ${row}, ${col} hovered`);
+  if (this.board[row][col].value === '' && !this.gameOver) {
+    this.board[row][col].hovered = true;
+  }
+}
+
+onCellMouseLeave(row: number, col: number) {
+  this.board[row][col].hovered = false;
+}
+
 }
