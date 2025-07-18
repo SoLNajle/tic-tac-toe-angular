4e6d4503e5e3df9e2f44ba430a228c0837e32e97
Sol Najle <sgnajle@gmail.com>
2025-07-18 15:51:16 +0200

    added proper camelCase

--- src/app/services/game-logic.service.ts
+++ src/app/services/game-logic.service.ts
@@ -5,24 +5,24 @@ import { BoardModel } from '../models/board.model';
 export class GameLogicService {
     checkWinner(board: BoardModel): string | null {
         // Check rows
-        const winnerByRow = this.checkWinnerbyRow(board);
-        if (winnerByRow) {
-            return winnerByRow;
+        const WinnerByRow = this.checkWinnerByRow(board);
+        if (WinnerByRow) {
+            return WinnerByRow;
         }
         // Check columns
-        const winnerByColumn = this.checkWinnerbyColumn(board);
-        if (winnerByColumn) {
-            return winnerByColumn;
+        const WinnerByColumn = this.checkWinnerByColumn(board);
+        if (WinnerByColumn) {
+            return WinnerByColumn;
         }
         // Check diagonals
-        const winnerByDiagonal = this.checkWinnerbyDiagonal(board);
-        if (winnerByDiagonal) {
-            return winnerByDiagonal;
+        const WinnerByDiagonal = this.checkWinnerByDiagonal(board);
+        if (WinnerByDiagonal) {
+            return WinnerByDiagonal;
         }
         return null;
     }
 
-    checkWinnerbyRow(board: BoardModel): string | null {
+    checkWinnerByRow(board: BoardModel): string | null {
         for (let i = 0; i < 3; i++) {
             if (board[i][0].value === board[i][1].value && board[i][1].value === board[i][2].value && board[i][0].value !== '') {
                 return board[i][0].value;
@@ -30,7 +30,7 @@ export class GameLogicService {
         }
         return null;
     }
-    checkWinnerbyColumn(board: BoardModel): string | null {
+    checkWinnerByColumn(board: BoardModel): string | null {
         for (let i = 0; i < 3; i++) {
             if (board[0][i].value === board[1][i].value && board[1][i].value === board[2][i].value && board[0][i].value !== '') {
                 return board[0][i].value;
@@ -38,7 +38,7 @@ export class GameLogicService {
         }
         return null;
     }
-    checkWinnerbyDiagonal(board: BoardModel): string | null {
+    checkWinnerByDiagonal(board: BoardModel): string | null {
         if (board[0][0].value === board[1][1].value && board[1][1].value === board[2][2].value && board[0][0].value !== '') {
             return board[0][0].value;
         }
